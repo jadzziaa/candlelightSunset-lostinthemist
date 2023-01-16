f934ee3932e9136d774a70ecea2523615742dbe6
Dowdoon <filipstankovic7&@gmail.com>
2023-01-16 01:37:10 +0100

    added DrugDummies mod

---
+++ candlelightSunset - Lost in the Mist/Contents/mods/DrugDummies/media/lua/client/DrugDummies_Functions.lua
@@ -0,0 +1,11 @@
+--Functions For Drugs
+
+function OnSmoke_WeedBlunt(food, player)
+
+	local dmg = player:getBodyDamage();
+	local stats = player:getStats();
+	
+	stats:setBoredom(stats:getBoredom() - 55.5);
+	stats:setFear(stats:getFear() - 0.333);
+	stats:setDrunkenness(stats:getDrunkenness() + 50);
+end
